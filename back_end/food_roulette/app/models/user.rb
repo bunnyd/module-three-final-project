25219323481d4e14005ee4e0689eeb7b67a5402c
github username <github email address>
2018-09-11 17:46:32 -0500

    added yelp api

--- back_end/food_roulette/app/models/user.rb
+++ back_end/food_roulette/app/models/user.rb
@@ -1,3 +1,7 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
+
 class User < ApplicationRecord
   has_many :user_restaurants
   has_many :restaurants, through: :user_restaurants
@@ -7,10 +11,26 @@ class User < ApplicationRecord
   validates :username, :presence => true, :uniqueness => true
 
   def self.search(search_food_type, search_price_range, search_zip_code, search_rating)
-    yelpBaseURL = "https://api.yelp.com/v3/businesses/search?";
-    yelpAPIKey = "JXu7Wwa0miaPPT1CkKGrX97vdRQJG8cOOyDmG6OkYNmTs55lCGpfG1dyzJUTIjJhkzORD3yFWCWG-gvkttv6eoA5JMzqh5PghtvBtlpZBkwzgSro9YhQfW8aM9phW3Yx";
+    yelpBaseURL = "https://api.yelp.com/v3/businesses/search?"
+    yelpAPIKey = "JXu7Wwa0miaPPT1CkKGrX97vdRQJG8cOOyDmG6OkYNmTs55lCGpfG1dyzJUTIjJhkzORD3yFWCWG-gvkttv6eoA5JMzqh5PghtvBtlpZBkwzgSro9YhQfW8aM9phW3Yx"
     yelpURL = "#{yelpBaseURL}term=#{search_food_type}&price=#{search_price_range}&location=#{search_zip_code}&rating=#{search_rating}"
 
-    return yelpURL
+    response = RestClient.get(yelpURL, {'Authorization' => "Bearer #{yelpAPIKey}"  })
+    result = JSON.parse(response.body)
+
+
+    # uri = URI.parse(yelpURL)
+    # req = Net::HTTP::Get.new(uri)
+    # req.use_ssl = true
+    #
+    # req['Authorization'] = "Bearer #{yelpAPIKey}"
+    #
+    # res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+    #   byebug
+    #   http.request(req)
+    # }
+    #
+    # # response = Net::HTTP.get_response(req.)
+    # result = JSON.parse(res.body)
   end
 end
